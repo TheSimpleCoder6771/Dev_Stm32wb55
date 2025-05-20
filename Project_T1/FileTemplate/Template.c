38abf57da3a1053313b76e19a671d3383f8406db
ttm-SP <s.prabhudev@ttmrail.com.au>
2025-05-20 10:01:03 +1000

    ADD - Tempelates for .c and .h file

---
+++ Project_T1/FileTemplate/Template.c
@@ -0,0 +1,47 @@
+// ***************************************************************************************
+// Copy Rights messsage
+// ***************************************************************************************
+
+//
+// Include.
+//
+
+// This file's API
+
+// Standard includes
+
+// TTM includes
+
+// Third Party Includes
+
+//
+// Constatnts and Macro Definitions.
+//
+
+//
+// Type defines.
+//
+
+//
+// Private Prototype.
+//
+
+//
+// File Globals.
+//
+
+//
+// Public Function definitions.
+//
+
+//
+// Private Function definitions.
+//
+
+//
+// Debug Functions
+//
+
+//
+// End
+//
---
+++ Project_T1/FileTemplate/Template.h
@@ -0,0 +1,28 @@
+// ****************************************************************************************
+// Copy Rights
+// ***************************************************************************************
+
+#ifndef TEMPLATE_H
+#define TEMPLATE_H
+
+//
+// Includes
+//
+
+//
+// Constants and macro definitions.
+//
+
+//
+// Type definitions.
+//
+
+//
+// Globals.
+//
+
+//
+// Public Prototypes.
+//
+
+#endif // TEMPLATE_H
